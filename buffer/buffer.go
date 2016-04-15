a2b2c94211f078eac1f9b5c553cc9ecefbc979af
weedle1912 <weedle1912@gmail.com>
2016-04-15 11:10:32 +0200

    Clear channel for buffer.

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -1,21 +1,22 @@
 package buffer
 
 import (
-	"fmt"
+	//"fmt"
 	"time"
 
 	. "project.go/obj"
 )
 
-func Manager() (chan<- Message, chan<- Message, <-chan Message) {
+func Manager() (chan<- Message, chan<- Message, chan<- struct{}, <-chan Message) {
 	push_channel := make(chan Message)
 	pop_channel := make(chan Message)
+	clear_channel := make(chan struct{})
+
 
 	resend_channel := make(chan Message)
 	pop_success_channel := make(chan Message)
-	//clear_channel := make(chan bool)
 
-	buffer_map := make(map[int]chan<- Message)
+	buffer_map := make(map[int]chan<- struct{})
 
 	go func() {
 		for {
@@ -23,18 +24,21 @@ func Manager() (chan<- Message, chan<- Message, <-chan Message) {
 			case msg := <-push_channel:
 				buffer_map[msg.Hash()] = worker(msg, resend_channel, pop_success_channel)
 			case msg := <-pop_channel:
-				fmt.Println("Pop:", msg)
-				buffer_map[msg.Hash()] <- msg
+				buffer_map[msg.Hash()] <- struct{}{}
 			case msg := <-pop_success_channel:
 				delete(buffer_map, msg.Hash())
+			case <-clear_channel:
+				for _, value := range buffer_map {
+					value <-struct{}{};
+				}
 			}
 		}
 	}()
-	return push_channel, pop_channel, resend_channel
+	return push_channel, pop_channel, clear_channel, resend_channel
 }
 
-func worker(msg Message, resend_channel, pop_success_channel chan<- Message) chan<- Message {
-	pop_worker_channel := make(chan Message)
+func worker(msg Message, resend_channel, pop_success_channel chan<- Message) chan<- struct{} {
+	pop_worker_channel := make(chan struct{})
 	go func() {
 		for {
 			select {
